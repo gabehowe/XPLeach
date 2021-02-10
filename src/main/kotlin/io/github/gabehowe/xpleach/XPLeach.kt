8383d3b3bd138aca598c18895943505dba639778
MIdnightfury <31964173+MIdnightfury@users.noreply.github.com>
2021-02-10 18:06:42 -0500

    initial checkin

---
+++ src/main/kotlin/io/github/gabehowe/xpleach/XPLeach.kt
@@ -0,0 +1,13 @@
+package io.github.gabehowe.xpleach
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class XPLeach : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
